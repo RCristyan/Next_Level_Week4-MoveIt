70bde73bd43c3e09ec8f7d47708b739a8b99864e
RCristyan <rcristyan9@gmail.com>
2021-02-23 22:08:57 -0300

    Adicionado componente Profile.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d1ab21eecf171d5c47fb";
+/******/ 	var hotCurrentHash = "49dbaddd39be8745cd52";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile(){
+    return(
+        <div>
+            <img src="https://github.com/RCristyan.png" alt="Renan Cristyan"/>
+            <div>
+                <strong>Renan Cristyan</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
