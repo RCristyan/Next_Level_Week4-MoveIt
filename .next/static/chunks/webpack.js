ccb77a6bb07f6090d201c9c562e5a0b46b8e75e7
RCristyan <rcristyan9@gmail.com>
2021-02-24 21:29:05 -0300

    Melhorias no Countdown e no botao do componente.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "56d00ecbec40a94769ef";
+/******/ 	var hotCurrentHash = "3bff62be8ad670befc53";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
