ae47314b966753936f1cc0368cbd510f7d328a31
RCristyan <rcristyan9@gmail.com>
2021-02-24 22:45:14 -0300

    Implementado suporte a contexto de componentes React.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "47a6aae85d433f4663fa";
+/******/ 	var hotCurrentHash = "81ce65e60365a1688413";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
