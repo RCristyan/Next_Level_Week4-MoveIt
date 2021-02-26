b3c102841493f4d6f2a1fc5299c576e8bd037771
RCristyan <rcristyan9@gmail.com>
2021-02-26 19:16:11 -0300

    Adicionado audio de notificaçao.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e5a2f86d63fda7424d37";
+/******/ 	var hotCurrentHash = "9552ed23cff2d1551f6e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
