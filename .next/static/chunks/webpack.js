f0e9837105f95741f7254ecb45118a47734260c0
RCristyan <rcristyan9@gmail.com>
2021-02-26 19:13:42 -0300

    Adicionada notificaçao de novo desafio.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d0f9de4ca599f43186a5";
+/******/ 	var hotCurrentHash = "e5a2f86d63fda7424d37";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
