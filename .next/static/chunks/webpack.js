6ede46ff239df19b0dd0526b3de96be42239f771
RCristyan <rcristyan9@gmail.com>
2021-02-27 14:36:24 -0300

    level, currentExperience e challengesCompleted armazenadas em cookie.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9552ed23cff2d1551f6e";
+/******/ 	var hotCurrentHash = "43d1492bded26380b60d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
